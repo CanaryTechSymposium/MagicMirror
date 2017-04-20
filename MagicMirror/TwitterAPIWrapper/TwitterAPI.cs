9b3c86d3b0f5fcbfaf38ecf16462b3d8f4f8633d
Mark <Mark@ENGINEERING>
2017-04-20 10:10:39 -0400

    Changed application resolution to match display size

--- MagicMirror/TwitterAPIWrapper/TwitterAPI.cs
+++ MagicMirror/TwitterAPIWrapper/TwitterAPI.cs
@@ -1,77 +1,77 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Net;
-using Newtonsoft.Json;
-using System;
+﻿//using System.Collections.Generic;
+//using System.IO;
+//using System.Net;
+//using Newtonsoft.Json;
+//using System;
 
-namespace MagicMirror.ThirdParty
-{
-    public class TwitterAPI
-    {
-        string ConsumerKey = "";
-        string ConsumerSecret = "";
+//namespace MagicMirror.ThirdParty
+//{
+//    public class TwitterAPI
+//    {
+//        string ConsumerKey = "";
+//        string ConsumerSecret = "";
 
-        // constructor
-        public TwitterAPI(string key, string secret)
-        {
-            ConsumerKey = key;
-            ConsumerSecret = secret;
-        }
+//        // constructor
+//        public TwitterAPI(string key, string secret)
+//        {
+//            ConsumerKey = key;
+//            ConsumerSecret = secret;
+//        }
 
-        string WebRequest(string method, string url, string postData, Dictionary<string, string> headers)
-        {
+//        string WebRequest(string method, string url, string postData, Dictionary<string, string> headers)
+//        {
 
-            HttpWebRequest Request = null;
-            StreamWriter requestWriter = null;
+//            HttpWebRequest Request = null;
+//            StreamWriter requestWriter = null;
 
-            Request = (HttpWebRequest)HttpWebRequest.Create(url);
-            Request.Method = method.ToString();
+//            Request = (HttpWebRequest)HttpWebRequest.Create(url);
+//            Request.Method = method.ToString();
 
-            if (headers != null)
-            {
-                foreach (KeyValuePair<string, string> h in headers)
-                {
-                    Request.Headers.Add(h.Key, h.Value);
-                }
-            }
+//            if (headers != null)
+//            {
+//                foreach (KeyValuePair<string, string> h in headers)
+//                {
+//                    Request.Headers.Add(h.Key, h.Value);
+//                }
+//            }
 
 
-            if (method == "POST" || method == "DELETE")
-            {
-                Request.ContentType = "application/x-www-form-urlencoded";
+//            if (method == "POST" || method == "DELETE")
+//            {
+//                Request.ContentType = "application/x-www-form-urlencoded";
 
-                // POST the data.
-                requestWriter = new StreamWriter(Request.GetRequestStream());
-                try
-                {
-                    requestWriter.Write(postData);
-                }
-                finally
-                {
-                    requestWriter.Close();
-                    requestWriter = null;
-                }
-            }
+//                // POST the data.
+//                requestWriter = new StreamWriter(Request.GetRequestStream());
+//                try
+//                {
+//                    requestWriter.Write(postData);
+//                }
+//                finally
+//                {
+//                    requestWriter.Close();
+//                    requestWriter = null;
+//                }
+//            }
 
-            return (new StreamReader(Request.GetResponse().GetResponseStream())).ReadToEnd();
-        }
+//            return (new StreamReader(Request.GetResponse().GetResponseStream())).ReadToEnd();
+//        }
 
-        public string GetTrumpsFeed()
-        {
-            return WebRequest("GET", "https://api.twitter.com/1.1/search/tweets.json?q=@realDonaldTrump", "", new Dictionary<string, string> { { "Authorization", "Bearer " + GetBearerToken().access_token } });
-        }
+//        public string GetTrumpsFeed()
+//        {
+//            return WebRequest("GET", "https://api.twitter.com/1.1/search/tweets.json?q=@realDonaldTrump", "", new Dictionary<string, string> { { "Authorization", "Bearer " + GetBearerToken().access_token } });
+//        }
 
-        string GetAccessToken()
-        {
-            var enc = System.Text.Encoding.UTF8;
-            return Convert.ToBase64String(enc.GetBytes(WebUtility.UrlEncode(ConsumerKey) + ":" + WebUtility.UrlEncode(ConsumerSecret)));
-        }
+//        string GetAccessToken()
+//        {
+//            var enc = System.Text.Encoding.UTF8;
+//            return Convert.ToBase64String(enc.GetBytes(WebUtility.UrlEncode(ConsumerKey) + ":" + WebUtility.UrlEncode(ConsumerSecret)));
+//        }
 
-        TokenBearerResponse GetBearerToken()
-        {
-            var res = WebRequest("POST", "https://api.twitter.com/oauth2/token", "grant_type=client_credentials", new Dictionary<string, string> { { "Authorization", "Basic " + GetAccessToken() } });
-            return JsonConvert.DeserializeObject<TokenBearerResponse>(res);
-        }
+//        TokenBearerResponse GetBearerToken()
+//        {
+//            var res = WebRequest("POST", "https://api.twitter.com/oauth2/token", "grant_type=client_credentials", new Dictionary<string, string> { { "Authorization", "Basic " + GetAccessToken() } });
+//            return JsonConvert.DeserializeObject<TokenBearerResponse>(res);
+//        }
 
-    }
-}
+//    }
+//}
