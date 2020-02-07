9f6d97aab6af74de459a8a86b69e6451a6712ca6
Mark Rice <mrice@canarylabs.com>
2020-02-07 15:54:01 -0500

    MagicMirror: Updated countdown timer to include number of working days and hours remaining. The working day time is hard-coded for 8am - 5pm.

--- MagicMirror/CountDownTimer/CountDownTimer.xaml.cs
+++ MagicMirror/CountDownTimer/CountDownTimer.xaml.cs
@@ -32,6 +32,13 @@ namespace MagicMirror.CountDownTimer
             var countdownSettings = MagicMirror.Utilities.CredintialStore.GetCredintials("countdowntimer");
             Heading = countdownSettings.ID;
             _endDate = DateTime.Parse(countdownSettings.Secret);
+            _workdayStart = new TimeSpan(8, 0, 0);
+            _workdayEnd = new TimeSpan(17, 0, 0);
+            _workdayLength = _workdayEnd - _workdayStart;
+            //_vacationAndHolidays = new List<DateTime>()
+            //{
+            //    new DateTime(2020, 3, 6),
+            //};
 
             _updateTimer = new DispatcherTimer();
             _updateTimer.Interval = new TimeSpan(0, 0, 0, 1, 0);
@@ -42,6 +49,7 @@ namespace MagicMirror.CountDownTimer
         private void TimerTick(object sender, object e)
         {
             OnPropertyChanged(() => Duration);
+            OnPropertyChanged(() => WorkingDaysDuration);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -70,6 +78,105 @@ namespace MagicMirror.CountDownTimer
 
                 format += "h':'mm':'ss";
                 return duration.ToString(format);
+            }
+        }
+
+        private TimeSpan _workdayStart;
+        private TimeSpan _workdayEnd;
+        private TimeSpan _workdayLength;
+        private List<DateTime> _vacationAndHolidays;
+        public string WorkingDaysDuration
+        {
+            get
+            {
+                if (_endDate < DateTime.Now)
+                    return "";
+
+                var duration = GetBusinessTimespanBetween(DateTime.Now, _endDate, _workdayStart, _workdayEnd, _vacationAndHolidays);
+                double fractionalDays = duration / _workdayLength;
+                int days = (int)Math.Truncate(fractionalDays);
+                TimeSpan partialDay = _workdayLength * (fractionalDays - days);
+                string dayString = days > 1 ? $"{days} Working Days" : days > 0 ? "1 Working Day" : "Working Hours:";
+                return $"{dayString} {partialDay:h':'mm':'ss}";
+            }
+        }
+
+        private static TimeSpan GetBusinessTimespanBetween(
+            DateTime start, DateTime end,
+            TimeSpan workdayStartTime, TimeSpan workdayEndTime,
+            List<DateTime> holidays = null)
+        {
+            if (end < start)
+                throw new ArgumentException("start datetime must be before end datetime.");
+
+            // Just create an empty list for easier coding.
+            if (holidays == null) holidays = new List<DateTime>();
+
+            if (holidays.Where(x => x.TimeOfDay.Ticks > 0).Any())
+                throw new ArgumentException("holidays can not have a TimeOfDay, only the Date.");
+
+            var nonWorkDays = new List<DayOfWeek>() { DayOfWeek.Saturday, DayOfWeek.Sunday };
+
+            var startTime = start.TimeOfDay;
+
+            // If the start time is before the starting hours, set it to the starting hour.
+            if (startTime < workdayStartTime) startTime = workdayStartTime;
+
+            var timeBeforeEndOfWorkDay = workdayEndTime - startTime;
+
+            // If it's after the end of the day, then this time lapse doesn't count.
+            if (timeBeforeEndOfWorkDay.TotalSeconds < 0) timeBeforeEndOfWorkDay = new TimeSpan();
+            // If start is during a non work day, it doesn't count.
+            if (nonWorkDays.Contains(start.DayOfWeek)) timeBeforeEndOfWorkDay = new TimeSpan();
+            else if (holidays.Contains(start.Date)) timeBeforeEndOfWorkDay = new TimeSpan();
+
+            var endTime = end.TimeOfDay;
+
+            // If the end time is after the ending hours, set it to the ending hour.
+            if (endTime > workdayEndTime) endTime = workdayEndTime;
+
+            var timeAfterStartOfWorkDay = endTime - workdayStartTime;
+
+            // If it's before the start of the day, then this time lapse doesn't count.
+            if (timeAfterStartOfWorkDay.TotalSeconds < 0) timeAfterStartOfWorkDay = new TimeSpan();
+            // If end is during a non work day, it doesn't count.
+            if (nonWorkDays.Contains(end.DayOfWeek)) timeAfterStartOfWorkDay = new TimeSpan();
+            else if (holidays.Contains(end.Date)) timeAfterStartOfWorkDay = new TimeSpan();
+
+            // Easy scenario if the times are during the day day.
+            if (start.Date.CompareTo(end.Date) == 0)
+            {
+                if (nonWorkDays.Contains(start.DayOfWeek)) return new TimeSpan();
+                else if (holidays.Contains(start.Date)) return new TimeSpan();
+                return endTime - startTime;
+            }
+            else
+            {
+                var timeBetween = end - start;
+                var daysBetween = (int)Math.Floor(timeBetween.TotalDays);
+                var dailyWorkSeconds = (int)Math.Floor((workdayEndTime - workdayStartTime).TotalSeconds);
+
+                var businessDaysBetween = 0;
+
+                // Now the fun begins with calculating the actual Business days.
+                if (daysBetween > 0)
+                {
+                    var nextStartDay = start.AddDays(1).Date;
+                    var dayBeforeEnd = end.AddDays(-1).Date;
+                    for (DateTime d = nextStartDay; d <= dayBeforeEnd; d = d.AddDays(1))
+                    {
+                        if (nonWorkDays.Contains(d.DayOfWeek)) continue;
+                        else if (holidays.Contains(d.Date)) continue;
+                        businessDaysBetween++;
+                    }
+                }
+
+                var dailyWorkSecondsToAdd = dailyWorkSeconds * businessDaysBetween;
+
+                var output = timeBeforeEndOfWorkDay + timeAfterStartOfWorkDay;
+                output = output + new TimeSpan(0, 0, dailyWorkSecondsToAdd);
+
+                return output;
             }
         }
 
